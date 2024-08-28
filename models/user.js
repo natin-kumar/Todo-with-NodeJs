3e9a98dab288cc6f3c71fef6d5ca8cb648f78c14
natin-kumar <natinkumar1161@gmail.com>
2024-08-28 11:40:12 +0530

    Delete models directory

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose= require('mongoose');
-const userSchema= new mongoose.schema({
-    name:String,
-    mail:String,
-    password:String
-});
-const userData = mongoose.model('userData',userSchema);
-module.exports=userData
